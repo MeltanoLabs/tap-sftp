3f173ed5251123e83e364a98ce9bf8786b1e1ef9
Pat Nadolny <patnadolny@gmail.com>
2021-03-01 16:11:47 -0500

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as fh:
 
 setup(
     name="tap-sftp",
-    version="2.0.0",
+    version="2.0.1",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
