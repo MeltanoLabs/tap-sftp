3396596ac331fe3120e63cbe10e48525ee652246
Henning <henning.holgersen@gmail.com>
2022-09-21 20:38:55 +0000

    pass extra param

--- tap_sftp/sync.py
+++ tap_sftp/sync.py
@@ -63,7 +63,8 @@ def sync_file(sftp_file_spec, stream, table_spec, config, sftp_client):
     opts = {'key_properties': table_spec['key_properties'],
             'delimiter': table_spec.get('delimiter', ','),
             'file_name': sftp_file_spec['filepath'],
-            'encoding': table_spec.get('encoding', 'utf-8')}
+            'encoding': table_spec.get('encoding', 'utf-8'),
+            'sanitize_headers': table_spec.get('sanitize_headers', False)}
 
     readers = csv_handler.get_row_iterators(file_handle, options=opts, infer_compression=True)
 
