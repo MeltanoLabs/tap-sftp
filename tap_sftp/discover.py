6801f510e34f290e30003f7c2641fc8228ed3c4f
Henning <henning.holgersen@gmail.com>
2022-11-15 20:22:29 +0100

    tox tests passed

--- tap_sftp/discover.py
+++ tap_sftp/discover.py
@@ -17,8 +17,7 @@ def discover_streams(config):
         LOGGER.info('Sampling records to determine table JSON schema "%s".', table_spec['table_name'])
         schema = json_schema.get_schema_for_table(conn, table_spec, config)
         stream_md = metadata.get_standard_metadata(schema,
-                                                   key_properties=table_spec.get('key_properties', 
-                                                   ['_sdc_source_file', '_sdc_source_lineno']),
+                                                   key_properties=table_spec.get('key_properties', ['_sdc_source_file', '_sdc_source_lineno']),  # noqa
                                                    replication_method='INCREMENTAL')
         streams.append(
             {
